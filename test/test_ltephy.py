0203188fe00b4df532b1b1c1cbfbcaab39a0935c
Petri J. Väisänen <petri.j.vaisanen@gmail.com>
2015-07-17 19:09:45 +0300

    One OFDM symbol generation test added and Git ignore updated.

--- test/test_ltephy.py
+++ test/test_ltephy.py
@@ -7,15 +7,40 @@ def setup():
 def teardown():
     pass
 
-def test_ofdmsym():
+def test_ofdmsym_tx_rx_real():
     '''
-    Test:OfdmSym:tx and rx
+    Test:OfdmSym:test_ofdmsym_tx_rx_real
     '''
+    # Parameters
     params = {
         'nFFT': 128
     }
+    # OFDM symbol generator
     osg = OfdmSym(params)
+    # Input symbols
     syms = 2 * np.random.randint(2, size=params['nFFT']) - 1
+    # Chain
     tx = osg.tx(syms)
     rx = osg.rx(tx)
-    np.testing.assert_array_almost_equal(rx, syms, decimal=14)
+    # Test(s)
+    np.testing.assert_allclose(rx, syms)
+    #np.testing.assert_array_almost_equal(rx, syms, decimal=14)
+
+def test_ofdmsym_tx_rx_complex():
+    '''
+    Test:OfdmSym:test_ofdmsym_tx_rx_complex
+    '''
+    # Parameters
+    params = {
+        'nFFT': 2048
+    }
+    # OFDM symbol generator
+    osg = OfdmSym(params)
+    # Input symbols
+    syms = (2 * np.random.randint(2, size=params['nFFT']) - 1) + 1j*(2 * np.random.randint(2, size=params['nFFT']) - 1)
+    # Chain
+    tx = osg.tx(syms)
+    rx = osg.rx(tx)
+    # Test(s)
+    np.testing.assert_allclose(rx, syms)
+    #np.testing.assert_array_almost_equal(rx, syms, decimal=14)
